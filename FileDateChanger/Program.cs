14f049a7991b5fc2573360cdfc5888a64f110024
Ioannis Panagopoulos <ipplos@gmail.com>
2017-04-10 07:32:46 +0300

    -

--- FileDateChanger/Program.cs
+++ FileDateChanger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace FileDateChanger
     {
         static void Main(string[] args)
         {
+            foreach (var file in Directory.GetFiles(args[0],"*.*",SearchOption.AllDirectories).ToList())
+            {
+                DateTime ModifiedDate = File.GetLastWriteTime(file);
+                Console.WriteLine($"\t{Path.GetFileName(file)} - {ModifiedDate.ToShortDateString()+" "+ModifiedDate.ToShortTimeString()}");
+                File.SetCreationTime(file, ModifiedDate);
+            }
+
+            Console.ReadKey();
+            
         }
     }
 }
